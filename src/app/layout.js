6283c01c6c6b55400d761534b9b4b48d7223deb7
anasTalaat <an.talaat@amazing.sa>
2024-04-16 13:09:42 +0200

    Add Navbar

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Noto_Sans_Arabic } from "next/font/google";
+import "./globals.css";
+
+const noto = Noto_Sans_Arabic({ subsets: ["arabic"] });
+
+export const metadata = {
+  title: "Amazing",
+  description: "Amazing e-commerce",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body dir="rtl" className={noto.className}>{children}</body>
+    </html>
+  );
+}
