7a0546d95e5a51eae82e973e8bf97f5d2e005c40
anasTalaat <167093582+anasTalaat@users.noreply.github.com>
2024-05-28 09:59:07 +0300

    update orders page in dashboard

--- src/app/dashboard/Orders/Orders.jsx
+++ src/app/dashboard/Orders/Orders.jsx
@@ -28,8 +28,8 @@ const Orders = () => {
       <DashboardHeader />
           { orders ?
             <>
-              <div className='grid grid-cols-5 gap-8 w-full'>
-                <div className='p-3 w-[100%] h-[55px] flex items-center justify-start gap-3 rounded-3xl bg-[#FEF5A8] shadow-md'>
+              <div className='grid grid-cols-5 gap-10 w-full'>
+                <div className='p-3 w-[100%] h-[55px] flex items-center justify-start gap-3 rounded-3xl bg-[#FEF5A8] shadow-lg'>
                   <Image src={timer} alt='timer' className='w-[40px] h-[40px] bg-white p-2 rounded-3xl' />
                   <p className='text-lg'>طلبات قيد المراجعة</p>
                 </div>
@@ -65,7 +65,7 @@ const Orders = () => {
               <div className="flex flex-col w-full">
                 <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                   <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                    <div className="overflow-hidden rounded-3xl">
+                    <div className="overflow-hidden rounded-3xl border-[1px] border-[#F1F1F1]">
                       <table className="min-w-full text-center text-sm font-normal text-surface text-white">
                         <thead className="border-b border-neutral-200 bg-gradient-to-br from-[#252B42] to-[#5E6DA8]">
                           <tr>
@@ -90,7 +90,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={``} className="flex justify-start items-center gap-2 p-3">
-                                <p>1</p>
+                                <p className='text-base'>1</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" onClick={() => setOrders(false)} />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -113,7 +113,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>2</p>
+                                <p className='text-base'>2</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -136,7 +136,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>3</p>
+                                <p className='text-base'>3</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -159,7 +159,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>4</p>
+                                <p className='text-base'>4</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -182,7 +182,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>5</p>
+                                <p className='text-base'>5</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -205,7 +205,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>6</p>
+                                <p className='text-base'>6</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -228,7 +228,7 @@ const Orders = () => {
                           <tr className="text-center border-b-[1px] border-[#F1F1F1]">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>7</p>
+                                <p className='text-base'>7</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -251,7 +251,7 @@ const Orders = () => {
                           <tr className="text-center">
                             <td className="whitespace-nowrap">
                               <Link href={`/`} className="flex justify-start items-center gap-2 p-3">
-                                <p>8</p>
+                                <p className='text-base'>8</p>
                                 <Image src={userImg} alt="user" className="w-[40px] h-[40px]" />
                                 <span className="flex flex-col items-start gap-1">
                                   <p className="text-md font-medium">أحمد حسن</p>
@@ -292,7 +292,7 @@ const Orders = () => {
                 </span>
               </span>
             </div>
-            <div className='flex justify-center items-center gap-36 mt-12 p-4 border-[1px] border-[#F1F1F1] rounded-3xl'>
+            <div className='flex justify-center items-center gap-36 mt-12 p-4 border-[1px] border-[#F1F1F1] rounded-3xl text-base'>
               <span className='flex items-center gap-2'>
                 <Image src={itemImg} alt='item' className='w-[45px] h-[45px]' />
                 <p>بدلة كاروهات</p>
@@ -308,14 +308,14 @@ const Orders = () => {
                 </span>
                 <span className='flex flex-col justify-center items-center gap-3'>
                   <p>المقاس</p>
-                  <span className='text-center bg-[#5E6DA8] rounded-md text-white p-2'>
+                  <span className='text-center bg-[#5E6DA8] rounded-md text-white pt-2 pb-2 pl-3 pr-3'>
                     s
                   </span>
                 </span>
               </span>
               <p>500.00 ر.س</p>
             </div>
-            <div className='flex justify-center items-center gap-36 p-4 border-[1px] border-[#F1F1F1] rounded-3xl'>
+            <div className='flex justify-center items-center gap-36 p-4 border-[1px] border-[#F1F1F1] rounded-3xl text-base'>
               <span className='flex items-center gap-2'>
                 <Image src={itemImg} alt='item' className='w-[45px] h-[45px]' />
                 <p>بدلة كاروهات</p>
@@ -331,7 +331,7 @@ const Orders = () => {
                 </span>
                 <span className='flex flex-col justify-center items-center gap-3'>
                   <p>المقاس</p>
-                  <span className='text-center bg-[#5E6DA8] rounded-md text-white p-2'>
+                  <span className='text-center bg-[#5E6DA8] rounded-md text-white pt-2 pb-2 pl-3 pr-3'>
                     s
                   </span>
                 </span>
@@ -339,30 +339,30 @@ const Orders = () => {
               <p>500.00 ر.س</p>
             </div>
           </div>
-          <div className='w-1/4 h-full flex flex-col justify-center items-start gap-5 rounded-2xl relative order-status'>
+          <div className='w-1/4 h-full flex flex-col gap-5 rounded-2xl relative order-status'>
             <span className='w-full flex justify-between font-bold mt-[45px]'>
-                عدد المنتجات (2)
-                <p className='text-[#434343]'>1000 ر.س</p>
+              عدد المنتجات (2)
+              <p className='text-[#434343]'>1000 ر.س</p>
             </span>
             <span className='w-full flex justify-between font-bold'>
-                مصاريف الشحن
-                <p className='text-[#434343]'>40 ر.س</p>
+              مصاريف الشحن
+              <p className='text-[#434343]'>40 ر.س</p>
             </span>
             <span className='w-full flex justify-between font-bold'>
-                خصومات
-                <p className='text-[#434343]'>00.00 ر.س</p>
+              خصومات
+              <p className='text-[#434343]'>00.00 ر.س</p>
             </span>
             <span className='w-full flex justify-between gap-3 font-bold'>
-                <input type='text' className='w-3/4 p-2 rounded-xl outline-none border-[1px] border-[#CCCCCC] placeholder:text-xs placeholder:font-light placeholder:text-[#CCCCCC]' placeholder='ادخل كود الخصم' />
-                <button className='rounded-xl bg-[#5E6DA8] text-white p-3 w-1/4 font-light'>تطبيق</button>
+              <input type='text' className='w-3/4 p-1 rounded-xl outline-none border-[1px] border-[#CCCCCC] placeholder:text-xs placeholder:font-light placeholder:text-[#CCCCCC] placeholder:pr-1 font-light' placeholder='ادخل كود الخصم' />
+              <button className='text-sm rounded-xl bg-[#5E6DA8] text-white p-1 w-1/4 font-light'>تطبيق</button>
             </span>
             <hr className='bg-[#DEDEDE] text-[#DEDEDE] w-full h-[1px] rounded-md' />
             <span className='w-full flex justify-between font-bold'>
-                الإجمالي
-                <p className='text-[#434343]'>1040 ر.س</p>
+              الإجمالي
+              <p className='text-[#434343]'>1040 ر.س</p>
             </span>
             <div className='flex flex-col gap-3 w-full mt-20'>
-              <p className='font-medium'>حالة الطلب</p>
+              <p>حالة الطلب</p>
               <div className="p-6 w-full h-[35px] flex justify-center items-center gap-6 rounded-2xl bg-[#FEF5A8]">
                 <Image src={timer} alt="timer" className="w-[35px] h-[35px] bg-white p-2 rounded-3xl" />
                 <p>قيد المراجعة</p>
@@ -377,9 +377,9 @@ const Orders = () => {
         <div className='flex flex-col items-start gap-5'>
           <p className='font-medium text-lg'>تتبع الشحنة</p>
           <div className='flex gap-8 min-w-[14%]'>
-            <span className='flex flex-col justify- items-center gap-2'>
+            <span className='flex flex-col justify-center items-center gap-2'>
               <Image src={greenTickCircle} alt='done' className='w-[20px] h-[20px]' />
-              <Image src={dots} alt='dots' className='w-[20px] h-[30px]' />
+              <Image src={dots} alt='dots' className='w-[40px] h-[40px]' />
             </span>
             <div className="p-3 w-full h-[35px] flex justify-center items-center gap-3 rounded-2xl bg-[#FEF5A8]">
               <Image src={timer} alt="timer" className="w-[30px] h-[30px] bg-white p-2 rounded-3xl" />
@@ -389,7 +389,7 @@ const Orders = () => {
           <div className='flex gap-8 min-w-[14%]'>
             <span className='flex flex-col justify-center items-center gap-2'>
               <Image src={greenTickCircle} alt='done' className='w-[20px] h-[20px]' />
-              <Image src={dots} alt='dots' className='w-[20px] h-[30px]' />
+              <Image src={dots} alt='dots' className='w-[40px] h-[40px]' />
             </span>
             <div className="p-3 w-full h-[35px] flex justify-center items-center gap-3 rounded-2xl bg-[#FEE1A8]">
               <Image src={boxTime} alt="timer" className="w-[30px] h-[30px] bg-white p-2 rounded-3xl" />
@@ -399,7 +399,7 @@ const Orders = () => {
           <div className='flex gap-8 min-w-[14%]'>
             <span className='flex flex-col justify-center items-center gap-2'>
               <Image src={greenTickCircle} alt='done' className='w-[20px] h-[20px]' />
-              <Image src={dots} alt='dots' className='w-[20px] h-[30px]' />
+              <Image src={dots} alt='dots' className='w-[40px] h-[40px]' />
             </span>
             <div className="p-3 w-full h-[35px] flex justify-center items-center gap-3 rounded-2xl bg-[#F2FEA8]">
               <Image src={shipping} alt="timer" className="w-[30px] h-[30px] bg-white p-2 rounded-3xl" />
@@ -409,7 +409,7 @@ const Orders = () => {
           <div className='flex gap-8 min-w-[14%]'>
             <span className='flex flex-col justify-center items-center gap-2'>
               <Image src={greenTickCircle} alt='done' className='w-[20px] h-[20px]' />
-              <Image src={dots} alt='dots' className='w-[20px] h-[30px]' />
+              <Image src={dots} alt='dots' className='w-[40px] h-[40px]' />
             </span>
             <div className="p-3 w-full h-[35px] flex justify-center items-center gap-3 rounded-2xl bg-[#C9FEA8]">
               <Image src={boxTick} alt="timer" className="w-[30px] h-[30px] bg-white p-2 rounded-3xl" />
@@ -421,8 +421,8 @@ const Orders = () => {
               <Image src={grayTickCircle} alt='done' className='w-[20px] h-[20px]' />
             </span>
             <span className='relative'>
-              <Image src={loading} alt="loading" className="w-[50px] h-[50px] bg-white p-2 rounded-3xl" />
-              <span className='flex justify-center items-center gap-1 absolute top-2/4 -translate-y-2/4 -translate-x-2/4 -right-1'>
+              <Image src={loading} alt="loading" className="w-[70px] h-[50px] bg-white p-2 rounded-3xl" />
+              <span className='flex justify-center items-center gap-1 absolute top-2/4 -translate-y-2/4 -translate-x-2/4 right-2'>
                 <Image src={loadingDot} alt="loading" className="w-[7px] h-[7px]" />
                 <Image src={loadingDot} alt="loading" className="w-[7px] h-[7px]" />
                 <Image src={loadingDot} alt="loading" className="w-[7px] h-[7px]" />
