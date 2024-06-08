91a78b6927cfc9d31ddab83cca2d591a166aa76c
anasTalaat <167093582+anasTalaat@users.noreply.github.com>
2024-06-08 14:25:37 +0300

    Implemented responsive design for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,8 +17,8 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <nav className='flex flex-col relative z-50'>
-        <div className={`h-[50px] p-5 flex ${pathname === '/products' ? 'flex-row' : 'flex-row-reverse'} justify-between items-center pb-5 pt-5 pr-[60px] pl-[60px]`}>
-            <div className={`flex flex-row-reverse gap-3 items-center ${pathname !== '/products' && 'mr-auto'}`}>
+        <div className={`h-[50px] p-5 max-md:p-2 flex ${pathname === '/products' ? 'flex-row' : 'flex-row-reverse'} justify-between items-center md:pr-[60px] md:pl-[60px]`}>
+            <div className={`flex flex-row-reverse gap-3 max-md:gap-[2px] items-center ${pathname !== '/products' && 'mr-auto'}`}>
                 <Link href='https://www.facebook.com' target={'_blank'}>
                     <FontAwesomeIcon icon={faFacebook} className='duration-200 hover:text-blue-400 text-blue-500' />
                 </Link>
@@ -45,7 +45,7 @@ const Navbar = () => {
                 <div className='profile flex flex-row-reverse items-center gap-2'>
                     <Link className='flex flex-row-reverse gap-5 items-center' href='/profile'>
                         <Image src={ProfileImg} className='w-[40px] h-[40px] rounded-full' alt='Amazing' />
-                        <p className='text-white text-[12px]'>أحمد حسن</p>
+                        <p className='text-black text-[12px]'>أحمد حسن</p>
                     </Link>
                     <span className='flex flex-row-reverse gap-3'>
                         <Link className='relative flex flex-row-reverse gap-5 items-center' href='cart'>
@@ -64,12 +64,12 @@ const Navbar = () => {
                 </div>
             :
                 <span className='relative'>
-                    <input type='text' placeholder='بحث عن منتج' className='p-1 w-[180%] border-2 border-gray-300 rounded-full outline-none' />
-                    <FontAwesomeIcon icon={faSearch} className='w-[16px] h-[16px] text-gray-300 absolute left-[-120px] top-2/4 -translate-y-2/4 -translate-x-2/4' />
+                    <input type='text' placeholder='بحث عن منتج' className='p-1 w-[180%] max-md:w-full border-2 border-gray-300 rounded-full outline-none' />
+                    <FontAwesomeIcon icon={faSearch} className='w-[16px] h-[16px] text-gray-300 absolute left-[-120px] max-md:left-4 top-2/4 -translate-y-2/4 -translate-x-2/4' />
                 </span>
             }
         </div>
-        <div className='nav bg-gradient-to-br from-[#252B42] to-[#5E6DA8] h-[71px] p-5 flex flex-row justify-between pb-5 pt-5 pr-[60px] pl-[60px]'>
+        <div className='nav bg-gradient-to-br from-[#252B42] to-[#5E6DA8] h-[71px] max-md:h-[140px] p-5 flex flex-row justify-between max-md:flex-col max-md:justify-center max-md:items-center max-md:gap-2 pb-5 pt-5 pr-[60px] pl-[60px]'>
             <Link href='/'>
                 <Image src={Logo} className='w-[170px] h-[38px] ml-auto' alt='Amazing' />
             </Link>
@@ -79,9 +79,9 @@ const Navbar = () => {
                 <Link className='relative' href='/'>دولاب أطفالي</Link>
             </div>
         { pathname === '/products' ?
-            <span className='relative w-[15%]'>
+            <span className='relative w-[15%] max-md:w-full'>
                 <input type='text' placeholder='بحث عن منتج' className='w-full p-2 text-sm text-white placeholder:text-[#C1C1C1] placeholder:pr-2 bg-transparent border-[1px] border-[#C1C1C1] rounded-full outline-none' />
-                <FontAwesomeIcon icon={faSearch} className='w-[16px] h-[16px] text-gray-300 absolute left-5 top-[60%] -translate-y-2/4 -translate-x-2/4' />
+                <FontAwesomeIcon icon={faSearch} className='w-[16px] h-[16px] text-gray-300 absolute left-5 top-[60%] md:top-[55%] -translate-y-2/4 -translate-x-2/4' />
             </span>
         :
             <div className='profile flex flex-row-reverse items-center gap-5'>
