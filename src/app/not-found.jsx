62c1f755cbd7505978c088bc227d1a7bfd9150ab
anasTalaat <167093582+anasTalaat@users.noreply.github.com>
2024-06-29 11:14:15 +0300

    Update not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Navbar from '../components/Navbar/Navbar'
 import notFoundImg from '../assets/dashboard/notFound.svg'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 export default function Custom404() {
     return (
@@ -10,7 +12,10 @@ export default function Custom404() {
         <div className='flex flex-col justify-center items-center gap-3'>
           <Image src={notFoundImg} alt='Not Found' className='w-[400px] h-2/4' />
           <h2 className='text-3xl text-black'>هذا الرابط لا يوجد</h2>
-          <Link href='/' className='text-2xl text-[#5E6DA8] hover:text-[#5e6da8d7] duration-200'>الصفحة الرئيسية</Link>
+          <span className='flex items-center gap-3'>
+            <FontAwesomeIcon icon={faGear} className='animate-spin text-2xl text-[#5e6da8]' />
+            <Link href='/' className='font-bold text-2xl text-[#5e6da8d5] hover:text-[#5e6da8b7] duration-200'>الصفحة الرئيسية</Link>
+          </span>
         </div>
       </main>
     )
