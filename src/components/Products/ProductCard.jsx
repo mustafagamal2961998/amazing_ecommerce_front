2dbc2747b2b859d60206bbb803ecdd95365b1be3
anasTalaat <167093582+anasTalaat@users.noreply.github.com>
2024-07-04 10:09:52 +0300

    Update product card

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -9,6 +9,17 @@ import { useState } from "react"
 
 const ProductCard = (props) => {
   const [favImg, setFavImg] = useState(fav);
+
+  const [showFullTitle, setShowFullTitle] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowFullTitle(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullTitle(false);
+  };
+
   return (
     <div className='product relative w-full flex flex-col justify-center gap-5 rounded-xl shadow-xl'>
         <Image src={favImg} onMouseOver={() => setFavImg(redFav)} onMouseOut={() => setFavImg(fav)} width={20} height={20} className='absolute left-[20px] top-[15px] cursor-pointer w-[20px] h-[20px] object-cover z-50' alt={props.title} />
@@ -16,7 +27,7 @@ const ProductCard = (props) => {
             <Image src={props.thumbnail} width={240} height={270} className='thumbnail w-full h-[200px] rounded-md object-cover duration-200 hover:opacity-80' alt={props.title} />
         </Link>
         <span className='flex flex-col p-1'>
-            <p className='font-bold z-50'>{props.title}</p>
+            <p className='font-bold z-50' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{showFullTitle ? props.title : `${props.title.slice(0, 10)} ...`}</p>
             <span className='flex justify-between max-md:justify-center max-md:flex-col items-center w-full p-1'>
                 <span className='flex items-center gap-2 max-md:flex-col max-md:justify-center max-md:items-center'>
                     <p className='font-bold'>{props.price} ر.س</p>
