57bdeff79d6f90e17e6ae29d593cb1b87de1589a
anasTalaat <167093582+anasTalaat@users.noreply.github.com>
2024-08-07 12:49:19 +0300

    Update profile page

--- src/app/profile/Orders/Khazanty/Khazanty.jsx
+++ src/app/profile/Orders/Khazanty/Khazanty.jsx
@@ -35,7 +35,7 @@ const Khazanty = () => {
                         </span>
                     </span>
                 </div>
-                <div className='w-1/6 max-md:w-3/4 flex flex-col items-center justify-center gap-3'>
+                <div className='w-1/4 max-md:w-3/4 flex flex-col items-center justify-center gap-3'>
                     <span className='relative w-full h-[40px] shadow-lg bg-[#FF9500] p-2 rounded-br-full rounded-tl-full select-none cursor-pointer'>
                         <h2 className='w-max select-none text-lg text-white max-md:text-base absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4'>تعديل</h2>
                     </span>
@@ -70,7 +70,7 @@ const Khazanty = () => {
                         </span>
                     </span>
                 </div>
-                <div className='w-1/6 max-md:w-3/4 flex flex-col items-center justify-center gap-3'>
+                <div className='w-1/4 max-md:w-3/4 flex flex-col items-center justify-center gap-3'>
                     <span className='relative w-full h-[40px] shadow-lg bg-[#FF9500] p-2 rounded-br-full rounded-tl-full select-none cursor-pointer'>
                         <h2 className='w-max select-none text-lg text-white max-md:text-base absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4'>تعديل</h2>
                     </span>
