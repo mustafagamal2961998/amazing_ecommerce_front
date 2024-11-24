a50c58a1b29249649de0b2a6d14e302d55b004bd
anasTalaat <167093582+anasTalaat@users.noreply.github.com>
2024-11-24 10:09:53 +0200

    update component

--- src/app/dashboard/Reports/NotFoundComp.jsx
+++ src/app/dashboard/Reports/NotFoundComp.jsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
-import notFoundImg from '../../../assets/dashboard/notFound.svg';
 
 const NotFoundComp = () => {
   return (
-    <div className='w-full p-10 flex flex-col justify-center items-center gap-5 rounded-lg border-2 border-[#CFCFCF]'>
-        <Image src={notFoundImg} alt='not found' className='w-[300px] h-full' />
-        <p className='text-lg'>لا توجد بيانات</p>
+    <div className="w-full p-10 flex flex-col justify-center items-center gap-5 rounded-lg border-2 border-[#CFCFCF]">
+      <Image src="/assets/dashboard/notFound.svg" alt="not found" className="w-[300px] h-full" />
+      <p className="text-lg">لا توجد بيانات</p>
     </div>
-  )
+  );
 }
 
-export default NotFoundComp
+export default NotFoundComp;
